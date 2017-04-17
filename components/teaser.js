0851acb58b70e53d96a7be0f16fdc4c0532870e8
Matt Brennan <matt@153.io>
2017-04-17 13:18:09 +0100

    better content formatting, dates

--- components/teaser.js
+++ components/teaser.js
@@ -1,10 +1,12 @@
 import {withBreakpoints, bp} from 'react-element-breakpoints';
 import styled, {ThemeProvider} from 'styled-components';
-import {Heading, Paragraph} from './typography';
-import {baseSize} from './type-scale';
+import * as typography from './typography';
+import {sansScale, baseSize} from './type-scale';
 import {grey, teal} from '@quarterto/colours';
 import colour, {setColour} from './colour';
-import Link from '../components/link';
+import WickLink from '../components/link';
+import Markdown from 'react-markdown';
+import textRule from './text-rule';
 
 const teaserBreakpoints = withBreakpoints({
 	m: ({width}) => (width / baseSize) > 20,
@@ -12,6 +14,9 @@ const teaserBreakpoints = withBreakpoints({
 });
 
 const Teaser = styled.div`
+display: flex;
+overflow: hidden;
+
 ${colour('grey', 6)}
 border-left: 2px ${teal[3]} solid;
 padding: 1rem;
@@ -26,15 +31,51 @@ a:active & {
 }
 `;
 
-const getExcerpt = content => content.slice(0, 50) + (content.length > 50 ? '…' : '');
+const TeaserHeading = styled.div`
+flex: 1;
+`;
 
-export default teaserBreakpoints(({title, content, slug, breakpoints}) =>
-<Link simple href={{pathname: 'page', query: {slug}}} as={slug}>
+const Excerpt = styled(Markdown)`
+flex: 1;
+color: ${grey[3]};
+max-height: ${bp(6, {
+	l: 12,
+	m: 8
+})}rem;
+`;
+
+const Ruled = styled.span`
+color: ${grey[3]};
+${textRule(grey[3], grey[6])}
+`;
+
+const Heading = props => <typography.Heading {...props} anchor={false} level={Math.min(6, props.level + 4)} />;
+const Link = ({children}) => <Ruled>{children}</Ruled>;
+
+const SmallParagraph = styled.p`
+color: ${grey[3]};
+${sansScale(-3)};
+`;
+
+export default teaserBreakpoints(({title, content, slug, breakpoints, ...page}) =>
+<WickLink simple href={{pathname: 'page', query: {slug}}} as={slug}>
 	<Teaser>
-		<Heading anchor={false} level={bp(5, {
-			l: 3,
-			m: 4,
-		})({breakpoints})}>{title}</Heading>
-		<Paragraph>{getExcerpt(content)}</Paragraph>
+		<TeaserHeading>
+			<typography.Heading anchor={false} level={bp(5, {
+				l: 3,
+				m: 4,
+			})({breakpoints})}>{title}</typography.Heading>
+
+			<SmallParagraph>
+				{page.created && <time dateTime={new Date(page.created).toISOString()}>Created: {new Date(page.created).toLocaleString()}</time>}
+			</SmallParagraph>
+			<SmallParagraph>
+				{page.lastUpdated && <time dateTime={new Date(page.lastUpdated).toISOString()}>Last updated: {new Date(page.lastUpdated).toLocaleString()}</time>}
+			</SmallParagraph>
+		</TeaserHeading>
+
+		<Excerpt breakpoints={breakpoints} source={content} renderers={Object.assign({}, typography, {
+			Heading, Link
+		})} />
 	</Teaser>
-</Link>);
+</WickLink>);
